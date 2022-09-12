0a0531649a364179c05ee41c9ae1e998b2fd3178
Treavor Gagne <gagne.16@hotmail.com>
2022-09-12 08:50:15 -0700

    localstorage and lists

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ChakraProvider,
-  Grid,
   Box,
   VStack,
   theme,
@@ -11,7 +10,6 @@ import {
   Stack,
   Button,
   Input,
-  Heading,
   HStack,
   Tbody,
   Thead,
@@ -20,10 +18,13 @@ import {
   Th,
   Td,
   Table,
+  Checkbox,
 } from '@chakra-ui/react';
 import { Header } from './components/Header';
 
 function App() {
+  const [recipe, setRecipe] = useState('');
+
   const [directions, setDirections] = useState([]);
   const [step, setStep] = useState('');
 
@@ -32,6 +33,34 @@ function App() {
   const [ingredientAmount, setIngredientAmount] = useState(1);
   const [ingredientUnit, setIngredientUnit] = useState('tbs');
   const [ingredientName, setIngredientName] = useState('');
+
+  useEffect(() => {
+    const savedStateDirections = window.localStorage.getItem('directions');
+    if (savedStateDirections !== null)
+      setDirections(JSON.parse(savedStateDirections));
+
+    const savedStateIngredients = window.localStorage.getItem('ingredients');
+    if (savedStateIngredients !== null)
+      setIngredients(JSON.parse(savedStateIngredients));
+
+    const recipeState = window.localStorage.getItem('recipe');
+    console.log('load state', recipeState);
+    if (recipe !== null) setRecipe(recipeState);
+  }, []);
+
+  useEffect(() => {
+    const newIngredient = JSON.stringify([...ingredients]);
+    localStorage.setItem('ingredients', newIngredient);
+  }, [ingredients]);
+
+  useEffect(() => {
+    const newDirections = JSON.stringify([...directions]);
+    localStorage.setItem('directions', newDirections);
+  }, [directions]);
+
+  useEffect(() => {
+    localStorage.setItem('recipe', recipe);
+  }, [recipe]);
 
   const handleIngredientAdd = () => {
     if (ingredientName !== '') {
@@ -97,32 +126,61 @@ function App() {
     });
   };
 
+  const handleRecipeName = ({ target }) => {
+    setRecipe(target.value);
+  };
+
   const handleSubmit = () => {
-    console.log('SUBMIT MOTHER FUCKERS');
+    console.log(`Making ${recipe} mother fuckers`);
   };
 
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <VStack>
-            <Header />
-            <Box w={'70%'} mx={'auto'}>
+      <Box textAlign="center" p={2}>
+        <VStack maxH="100vh">
+          <Header />
+          <Stack
+            direction={['column', 'column', 'column', 'row']}
+            align="top"
+            w="95%"
+            mx={'auto'}
+            spacing={4}
+          >
+            <Box w={'100%'}>
               <form onSubmit={handleSubmit}>
                 <FormControl>
                   <Stack>
                     <FormLabel>Type</FormLabel>
-                    <Select isRequired placeholder="Select Recipe Type">
-                      <option>Dinner</option>
-                      <option>Lunch</option>
-                      <option>Breakfast</option>
-                      <option>Snack</option>
-                    </Select>
+                    <Stack justifyContent={'space-around'} direction="row">
+                      <Checkbox size="lg" value="Dinner">
+                        Dinner
+                      </Checkbox>
+                      <Checkbox size="lg" value="Lunch">
+                        Lunch
+                      </Checkbox>
+                      <Checkbox size="lg" value="Breakfast">
+                        Breakfast
+                      </Checkbox>
+                      <Checkbox size="lg" value="Snack">
+                        Snack
+                      </Checkbox>
+                    </Stack>
+                    <FormLabel>Recipe Name</FormLabel>
+                    <Input
+                      isRequired
+                      w="100%"
+                      onChange={handleRecipeName}
+                      type={'text'}
+                      name="recipeName"
+                      id="recipeName"
+                      placeholder="Tacos"
+                      value={recipe}
+                    ></Input>
                     <FormLabel>Ingredients</FormLabel>
                     <HStack>
                       <Select
                         isRequired
-                        w={'20%'}
+                        w={'33%'}
                         onChange={handleOptionChange}
                         name="ingredientOption"
                         id="ingredientOption"
@@ -132,7 +190,7 @@ function App() {
                       </Select>
                       <Input
                         isRequired
-                        w="10%"
+                        w="33%"
                         value={ingredientAmount}
                         onChange={handleAmountChange}
                         type={'number'}
@@ -142,7 +200,7 @@ function App() {
                       <Select
                         value={ingredientUnit}
                         isRequired
-                        w={'20%'}
+                        w={'34%'}
                         onChange={handleUnitChange}
                         name="ingredientUnit"
                         id="ingredientUnit"
@@ -151,7 +209,9 @@ function App() {
                         <option value="tps">tps</option>
                         <option value="mL">mL</option>
                         <option value="L">L</option>
-                        <option value="grams">grams</option>
+                        <option value="grams">g</option>
+                        <option value="large">kg</option>
+                        <option value="large">oz</option>
                         <option value="pinch">pinch</option>
                         <option value="dash">dash</option>
                         <option value="small">small</option>
@@ -159,135 +219,114 @@ function App() {
                         <option value="large">large</option>
                         <option value=""></option>
                       </Select>
-                      <Input
-                        isRequired
-                        w="100%"
-                        onChange={handleNameChange}
-                        type={'text'}
-                        name="ingredientName"
-                        id="ingredientName"
-                        placeholder="Ingredient Name"
-                      />
                     </HStack>
+                    <Input
+                      w="100%"
+                      onChange={handleNameChange}
+                      type={'text'}
+                      name="ingredientName"
+                      id="ingredientName"
+                      placeholder="Salt and Pepper"
+                    />
                     <Button fontSize={'18px'} onClick={handleIngredientAdd}>
                       {' '}
                       Add Ingredient{' '}
                     </Button>
-                    <Box mx={'auto'} textAlign="left">
-                      <Stack w={'100%'} direction={'column'}>
-                        <Heading
-                          textAlign={'left'}
-                          fontWeight={'500'}
-                          fontSize={18}
-                          color={'teal'}
-                        >
-                          Ingredient List
-                        </Heading>
-
-                        <TableContainer>
-                          <Table
-                            size={'sm'}
-                            variant="striped"
-                            colorScheme="teal"
-                          >
-                            <Thead>
-                              <Tr>
-                                <Th>Required</Th>
-                                <Th isNumeric>Amount</Th>
-                                <Th>Unit</Th>
-                                <Th>Ingredient</Th>
-                                <Th>Delete</Th>
-                              </Tr>
-                            </Thead>
-                            <Tbody>
-                              {ingredients.map((ingredient, index) => (
-                                <Tr>
-                                  <Td>{ingredient.required ? 'Yes' : 'No'}</Td>
-                                  <Td isNumeric>{ingredient.amount}</Td>
-                                  <Td>{ingredient.metric}</Td>
-                                  <Td>{ingredient.ingredient}</Td>
-                                  <Td>
-                                    <Button
-                                      size={'sm'}
-                                      colorScheme={'red'}
-                                      onClick={() =>
-                                        handleIngredientDelete(index)
-                                      }
-                                      justifySelf={'right'}
-                                    >
-                                      X
-                                    </Button>
-                                  </Td>
-                                </Tr>
-                              ))}
-                            </Tbody>
-                          </Table>
-                        </TableContainer>
-                      </Stack>
-
-                      <Stack>
-                        <Input
-                          w="100%"
-                          onChange={handleStepChange}
-                          type={'text'}
-                          name="step"
-                          id="step"
-                          placeholder="Direction Instructions"
-                        />
-                        <Button fontSize={'18px'} onClick={handleAddDirection}>
-                          Add Direction
-                        </Button>
-                        <Heading
-                          textAlign={'left'}
-                          fontWeight={'500'}
-                          fontSize={18}
-                          color={'teal'}
-                        >
-                          Recipe Directions
-                        </Heading>
-                        <TableContainer>
-                          <Table
-                            size={'sm'}
-                            variant="striped"
-                            colorScheme="teal"
-                          >
-                            <Thead>
-                              <Tr>
-                                <Th>Step</Th>
-                                <Th>Direction</Th>
-                                <Th>Delete</Th>
-                              </Tr>
-                            </Thead>
-                            <Tbody>
-                              {directions.map((step, index) => (
-                                <Tr>
-                                  <Td>{index + 1}</Td>
-                                  <Td>{step}</Td>
-                                  <Td isNumeric>
-                                    <Button
-                                      size={'sm'}
-                                      colorScheme={'red'}
-                                      onClick={() =>
-                                        handleDirectionsDelete(index)
-                                      }
-                                      justifySelf={'right'}
-                                    >
-                                      X
-                                    </Button>
-                                  </Td>
-                                </Tr>
-                              ))}
-                            </Tbody>
-                          </Table>
-                        </TableContainer>
-                      </Stack>
-                    </Box>
+                    <FormLabel>Directions</FormLabel>
+                    <Input
+                      w="100%"
+                      onChange={handleStepChange}
+                      type={'text'}
+                      name="step"
+                      id="step"
+                      placeholder="Direction Instructions"
+                    />
+                    <Button fontSize={'18px'} onClick={handleAddDirection}>
+                      Add Direction
+                    </Button>
+                    <Button
+                      fontSize={'18px'}
+                      colorScheme={'green'}
+                      onClick={handleSubmit}
+                    >
+                      Submit
+                    </Button>
                   </Stack>
                 </FormControl>
               </form>
             </Box>
-          </VStack>
-        </Grid>
+            <Box w={'100%'}>
+              <Stack direction={'column'}>
+                <FormLabel> Ingredient List</FormLabel>
+                <Box maxH={'35vh'} overflow="scroll">
+                  <TableContainer>
+                    <Table size={'sm'} variant="striped" colorScheme="teal">
+                      <Thead>
+                        <Tr>
+                          <Th>Required</Th>
+                          <Th isNumeric>Amount</Th>
+                          <Th>Unit</Th>
+                          <Th>Ingredient</Th>
+                          <Th isNumeric>Delete</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        {ingredients.map((ingredient, index) => (
+                          <Tr key={ingredient.ingredient + index}>
+                            <Td>{ingredient.required ? 'Yes' : 'No'}</Td>
+                            <Td isNumeric>{ingredient.amount}</Td>
+                            <Td>{ingredient.metric}</Td>
+                            <Td>{ingredient.ingredient}</Td>
+                            <Td isNumeric>
+                              <Button
+                                size={'xs'}
+                                colorScheme={'red'}
+                                onClick={() => handleIngredientDelete(index)}
+                              >
+                                X
+                              </Button>
+                            </Td>
+                          </Tr>
+                        ))}
+                      </Tbody>
+                    </Table>
+                  </TableContainer>
+                </Box>
+                <FormLabel> Recipe Directions</FormLabel>
+                <Box maxH={'35vh'} overflow="scroll">
+                  <TableContainer>
+                    <Table size={'sm'} variant="striped" colorScheme="teal">
+                      <Thead>
+                        <Tr>
+                          <Th>Step #</Th>
+                          <Th>Direction</Th>
+                          <Th isNumeric>Delete</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        {directions.map((step, index) => (
+                          <Tr key={step}>
+                            <Td>{index + 1}</Td>
+                            <Td>{step}</Td>
+                            <Td isNumeric>
+                              <Button
+                                size={'xs'}
+                                colorScheme={'red'}
+                                onClick={() => handleDirectionsDelete(index)}
+                              >
+                                X
+                              </Button>
+                            </Td>
+                          </Tr>
+                        ))}
+                      </Tbody>
+                    </Table>
+                  </TableContainer>
+                </Box>
+              </Stack>
+            </Box>
+          </Stack>
+        </VStack>
       </Box>
     </ChakraProvider>
   );
